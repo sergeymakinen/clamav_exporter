5d1e588c2ed8982bac29453178f708970bf014a7
Sergey Makinen <sergey@makinen.ru>
2025-02-07 17:47:38 +0300

    Switch to official ClamAV Docker image

--- exporter/clamd_test.go
+++ exporter/clamd_test.go
@@ -5,7 +5,9 @@ import (
 	"io/ioutil"
 	"net/url"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -57,11 +59,30 @@ func TestExporter_Collect_Clamd(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping TestExporter_Collect_Clamd during short test")
 	}
+	cmd := exec.Command("docker", "compose", "-f", "../testdata/docker/compose.yml", "exec", "clamav", "clamdscan", "-V")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	parts := strings.Split(strings.TrimSpace(string(out)), "/")
+	if len(parts) < 3 {
+		t.Fatalf("failed to parse clamdscan -V output:\n%s", out)
+	}
+	ver, err := strconv.Atoi(parts[1])
+	if err != nil {
+		t.Fatal(err)
+	}
+	tm, err := time.Parse("Mon Jan 2 15:04:05 2006", parts[2])
+	if err != nil {
+		t.Fatal(err)
+	}
 	address, _ := url.Parse("tcp://127.0.0.1:3310")
 	b, err := ioutil.ReadFile("testdata/metrics-integration.txt")
 	if err != nil {
 		panic("failed to read testdata/metrics-integration.txt: " + err.Error())
 	}
+	b = bytes.ReplaceAll(b, []byte("__CLAMAV_DB_VERSION__"), []byte(strconv.Itoa(ver)))
+	b = bytes.ReplaceAll(b, []byte("__CLAMAV_DB_TIMESTAMP_SECONDS__"), []byte(strconv.FormatInt(tm.Unix(), 10)))
 	metricNames := []string{
 		"clamav_db_timestamp_seconds",
 		"clamav_db_version",
